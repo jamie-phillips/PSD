81a6a04ad93ce8e684b70a89fff2ae070922ef6b
Jamie Phillips <jmpjamie@gmail.com>
2024-06-27 12:17:18 +0100

    Added Set-PowerButton.ps1 script

---
+++ Scripts/CustomScripts/Set-PowerButton.ps1
@@ -0,0 +1,2 @@
+powercfg -setacvalueindex SCHEME_CURRENT 4f971e89-eebd-4455-a8de-9e59040e7347 7648efa3-dd9c-4e3e-b566-50f929386280 3
+powercfg -setdcvalueindex SCHEME_CURRENT 4f971e89-eebd-4455-a8de-9e59040e7347 7648efa3-dd9c-4e3e-b566-50f929386280 3
